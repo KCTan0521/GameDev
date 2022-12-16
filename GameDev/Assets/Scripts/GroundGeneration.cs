90c9681f1e4327e8304347c36b523346c176a9d5
JakeWiDeam <96896829+JakeWiDeam@users.noreply.github.com>
2022-12-16 19:28:33 +0800

    Infinity platform
    two platform at a time

---
+++ GameDev/Assets/Scripts/GroundGeneration.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class GroundGeneration : MonoBehaviour
+{
+    [SerializeField] private GameObject _ground;
+    private CinemachineVirtualCamera playerCam;
+    private float leftScreen;
+    private float orthoSize;
+    private float aspectRatio;
+    private float camEnd;
+    private float leftMostGround;
+    private List<GameObject> grounds = new List<GameObject> ();
+
+    private void Start()
+    {
+        
+        
+    }
+    private void Awake()
+    {
+        playerCam = GameObject.Find("PlayerCam").GetComponent<CinemachineVirtualCamera>();
+        orthoSize = playerCam.m_Lens.OrthographicSize;
+        aspectRatio = GetComponent<Camera>().aspect;
+        leftScreen = orthoSize * aspectRatio;
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject newGround = Instantiate(_ground, new Vector2(i * 20f, 0f), Quaternion.identity);
+            grounds.Add(newGround);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camEnd = GetComponent<Transform>().position.x - leftScreen;
+        leftMostGround = grounds[0].transform.position.x + 20f;
+        Debug.Log(camEnd);
+        if (camEnd > leftMostGround)
+        {
+            Destroy(grounds[0]);
+            grounds.RemoveAt(0);
+            GameObject newGround = Instantiate(_ground, new Vector2(leftMostGround + 40f, 0f), Quaternion.identity);
+            grounds.Add(newGround);
+        }
+    }
+}
