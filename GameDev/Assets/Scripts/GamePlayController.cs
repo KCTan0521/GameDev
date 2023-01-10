4bf788520f40100d2c587e382b34b3767080c968
KCTan0521 <88612730+KCTan0521@users.noreply.github.com>
2023-01-10 23:01:39 +0800

    fixed the bug in the pause button

--- GameDev/Assets/Scripts/GamePlayController.cs
+++ GameDev/Assets/Scripts/GamePlayController.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class GamePlayController : MonoBehaviour
 {
     private bool isGamePaused = false;
+    private PlayerBehaviour playerBehaviour;
+
+    private void Awake()
+    {
+        playerBehaviour = GameObject.FindObjectOfType<PlayerBehaviour>();
+    }
+
     public void PauseGame()
     {
         if (isGamePaused)
         {
             Time.timeScale = 1;
             isGamePaused = false;
+            playerBehaviour.enabled = true;
         }
         else
         {
             Time.timeScale = 0;
             isGamePaused = true;
+            playerBehaviour.enabled = false;
         }
     }
+
 }
