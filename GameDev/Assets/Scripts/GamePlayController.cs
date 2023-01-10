f82db9978bf4721ed5898939b33bc5fcee7c61c1
KCTan0521 <88612730+KCTan0521@users.noreply.github.com>
2023-01-10 23:25:00 +0800

    face some bugs in the restart & go back homepage button
    still fixing, will update later, if program cannot because of this, can revert back to previous version

--- GameDev/Assets/Scripts/GamePlayController.cs
+++ GameDev/Assets/Scripts/GamePlayController.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GamePlayController : MonoBehaviour
 {
     private bool isGamePaused = false;
     private PlayerBehaviour playerBehaviour;
-
+    private GameObject gameSetting;
     private void Awake()
     {
         playerBehaviour = GameObject.FindObjectOfType<PlayerBehaviour>();
+        gameSetting = GameObject.FindGameObjectWithTag("GameSetting");
     }
 
     public void PauseGame()
@@ -19,13 +21,25 @@ public class GamePlayController : MonoBehaviour
             Time.timeScale = 1;
             isGamePaused = false;
             playerBehaviour.enabled = true;
+            gameSetting.SetActive(true);
         }
         else
         {
             Time.timeScale = 0;
             isGamePaused = true;
             playerBehaviour.enabled = false;
+            gameSetting.SetActive(false);
         }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScreen");
+    }
+
+    public void GoBackHomePage()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
 }
