1c16d0fb3e2ba7a4b0ae11dc5a0481c025db2c38
KCTan0521 <88612730+KCTan0521@users.noreply.github.com>
2023-01-11 00:19:06 +0800

    fixed the restart & go back homepage button, add an empty game over scene
    the restart & go back homepage button will show behind the platform for some reasons, need to set sorting layer to them, and remember to run the game from either MainMenu or SampleScene, don't run the GameOver (because currently nothing inside)

--- GameDev/Assets/Scripts/GamePlayController.cs
+++ GameDev/Assets/Scripts/GamePlayController.cs
@@ -5,13 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class GamePlayController : MonoBehaviour
 {
-    private bool isGamePaused = false;
+    private bool isGamePaused;
     private PlayerBehaviour playerBehaviour;
-    private GameObject gameSetting;
-    private void Awake()
+    private GameObject[] gameSetting;
+    
+    void Awake()
     {
         playerBehaviour = GameObject.FindObjectOfType<PlayerBehaviour>();
-        gameSetting = GameObject.FindGameObjectWithTag("GameSetting");
+        gameSetting = GameObject.FindGameObjectsWithTag("GameSetting");
+
+    }
+
+    void Start()
+    {
+        gameSettingStatus(false);
+        playerBehaviour.enabled = true;
+        isGamePaused = false;
+        Time.timeScale = 1;
+    }
+
+    void gameSettingStatus(bool status)
+    {
+        foreach (GameObject gs in gameSetting)
+        {
+            gs.SetActive(status);
+        }
     }
 
     public void PauseGame()
@@ -21,20 +39,20 @@ public class GamePlayController : MonoBehaviour
             Time.timeScale = 1;
             isGamePaused = false;
             playerBehaviour.enabled = true;
-            gameSetting.SetActive(true);
+            gameSettingStatus(false);
         }
         else
         {
             Time.timeScale = 0;
             isGamePaused = true;
             playerBehaviour.enabled = false;
-            gameSetting.SetActive(false);
+            gameSettingStatus(true);
         }
     }
 
     public void RestartGame()
     {
-        SceneManager.LoadScene("SampleScreen");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void GoBackHomePage()
