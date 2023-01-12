b55bdf19179f30f1717a2cdd012e3aaf0d179633
KCTan0521 <88612730+KCTan0521@users.noreply.github.com>
2023-01-12 22:10:57 +0800

    rename samplescreen to gameplay, and other stuff

--- GameDev/Assets/Scripts/GameOverController.cs
+++ GameDev/Assets/Scripts/GameOverController.cs
@@ -2,22 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameOverController : MonoBehaviour
 {
     // the Home Button and Restart Button do not override all back to the prefab
+    
+    [SerializeField]
+    private GameObject distanceTravelled;
 
+    [SerializeField]
+    private GameObject timeSurvived;
+
+    private PlayerBehaviour playerBehaviour;
 
 
     private void Start()
     {
-
+        // playerBehaviour = GameObject.FindObjectOfType<PlayerBehaviour>();
+        distanceTravelled.GetComponent<TextMeshProUGUI>().text += playerBehaviour.GetComponent<Transform>().position.x;
+        timeSurvived.GetComponent<TextMeshProUGUI>().text += "88s";
     }
 
 
     public void RestartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("GamePlay");
     }
 
     public void GoBackHomePage()
--- GameDev/Assets/Scripts/MainMenuController.cs
+++ GameDev/Assets/Scripts/MainMenuController.cs
@@ -7,7 +7,7 @@ public class MainMenuController : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("GamePlay");
     }
 
 }
