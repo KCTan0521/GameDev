a19850e686c1a5646e2600b8b2f590b3cee57237
RonaldGeneral <gogo23993@gmail.com>
2023-01-17 22:24:35 +0800

    define high jump and long jump
    fixing bug of giant not doing long jump

--- GameDev/Assets/Scripts/MonsterGiant.cs
+++ GameDev/Assets/Scripts/MonsterGiant.cs
@@ -1,3 +1,4 @@
+﻿using static System.Random;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,12 @@ using UnityEngine.SceneManagement;
 public class MonsterGiant : MonoBehaviour
 {
 
-    public float highJumpX;
+    public float highJumpXTile;
     public float highJumpY;
+    public float highJumpTriggerDistance;
     public float longJumpX;
     public float longJumpY;
+    public float longJumpTriggerDistance;
 
     public float impulseForceX;
     public float recoverTime;
@@ -18,8 +21,8 @@ public class MonsterGiant : MonoBehaviour
     public float attackDistance;
     public Health healthSystem;
 
-    public Transform groundCheck;
     public float groundCheckRadius;
+    public Transform groundCheck;
     public LayerMask groundLayer;
 
     private bool isGrounded;
@@ -28,7 +31,9 @@ public class MonsterGiant : MonoBehaviour
     private bool isDrop = false;
     private bool isLaunch = false;
     private bool isStop = false;
+    private bool jumpType;
     private float isStopTimer = 0;
+    private double highJumpX;
 
     private Rigidbody2D myBody;
     private Rigidbody2D _player;
@@ -41,11 +46,12 @@ public class MonsterGiant : MonoBehaviour
 
     public void Awake()
     {
-        myBody = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        trans = GetComponent<Transform>();
+        myBody = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        trans = gameObject.GetComponent<Transform>();
         _player = GameObject.Find("Player").GetComponent<Rigidbody2D>();
         isAttack = false;
+        jumpType = getJumpType();
     }
 
     public void Start()
@@ -57,14 +63,26 @@ public class MonsterGiant : MonoBehaviour
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-        if (isGrounded && !isAttack /*&& (myBody.transform.x - _player.transform.x) <= attackDistance*/)
+        if (isGrounded && !isAttack)
         {
-            anim.SetBool("isSmash", true);
+            
 
             if (!isLaunch)
             {
                 //launch once and avoid jumping again
-                MonsterJump();
+
+                if ((myBody.transform.position.x - _player.transform.position.x) <= longJumpTriggerDistance && !jumpType)
+                {
+                    Debug.Log("long");
+                    MonsterJump(false);
+                }
+                else if ((myBody.transform.position.x - _player.transform.position.x) <= highJumpTriggerDistance && jumpType)
+                {
+                    Debug.Log("highjump");
+                    MonsterJump();
+                }
+                    
+
                 isLaunch = true;
             }
 
@@ -96,15 +114,11 @@ public class MonsterGiant : MonoBehaviour
 
     public void Update()
     {
-        //Debug.Log(gameObject.transform.position.x);
-
 
         if(isJumping)
         {
             //check velocity y to know if dropping down
             anim.SetFloat("speed_y", myBody.velocity.y);
-
-            Debug.Log(myBody.velocity);
         }
             
 
@@ -134,17 +148,28 @@ public class MonsterGiant : MonoBehaviour
     }
 
 
+    private bool getJumpType()
+    {
+        //random generate true(high jump), false(long jump)
+        System.Random gen = new System.Random();
+        int prob = gen.Next(100);
+        return prob < 50;
+    }
+
+
     private void MonsterJump(bool isHighJump = true)
     {
         //Launching
         if (isHighJump)
         {
-            myBody.AddForce(new Vector2(highJumpX, highJumpY), ForceMode2D.Impulse);
+            highJumpX = (-57.035 * (double)highJumpXTile) - 1.15;
+            myBody.AddForce(new Vector2((float)highJumpX, highJumpY), ForceMode2D.Impulse);
         } else
         {
             myBody.AddForce(new Vector2(longJumpX, longJumpY), ForceMode2D.Impulse);
         }
-        
+
+        anim.SetBool("isSmash", true);
         Debug.Log("Jump");
 
         isJumping = true;
