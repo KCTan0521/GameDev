033f19484ab9988982f0efb372fff7570796715d
RonaldGeneral <gogo23993@gmail.com>
2023-01-28 20:15:48 +0800

    Monster generation part 1
    all on ground

--- GameDev/Assets/Scripts/MonsterController.cs
+++ GameDev/Assets/Scripts/MonsterController.cs
@@ -12,7 +12,8 @@ public class MonsterController : MonoBehaviour
 
     [SerializeField]
     private Transform leftPos;
-    private const float distanceX = 18f;
+    public float distanceX;
+    public int monsterSpawnLimit;
 
     private int randomIndex;
 
@@ -30,17 +31,14 @@ public class MonsterController : MonoBehaviour
             // set the time gap for monster generation to 1 - 3 seconds
             yield return new WaitForSeconds(Random.Range(1, 3));
 
-            // will generate at most 3 monsters
-            // because when length is 2 it will execute the code as well
-            // so 3 monsters in total
-            if (GameObject.FindGameObjectsWithTag("Monster").Length <= 2)
+            if (GameObject.FindGameObjectsWithTag("Monster").Length <= monsterSpawnLimit)
             {
                 // to randonmly generate the monster
                 randomIndex = Random.Range(0, monsterReference.Length);
                 spawnedMonster = Instantiate(monsterReference[randomIndex]);
 
-                // add a distance between the character
-                spawnedMonster.transform.position = new Vector3(leftPos.position.x + distanceX, 0f, 0f);
+                // spawn the monster with on the ground with a certain distance away
+                spawnedMonster.transform.position = new Vector2(leftPos.position.x + distanceX, spawnedMonster.GetComponent<SpriteRenderer>().bounds.size.y / 2);
             }
             
         } // while loop
